33bae29334d457ced5d9c285170d670ae6230834
yi1lei6a3 <x.7machinegun@gmail.com>
2022-07-15 01:21:49 +0300

    add work with PSQL from Python

--- task solution/5.Работа с PostgreSQL из Python.py
+++ task solution/5.Работа с PostgreSQL из Python.py
@@ -1,28 +1,31 @@
 import psycopg2
 
-def create_db(conn):
-    pass
+with psycopg2.connect(database="", user="", password="") as conn:
+    with conn.cursor() as cur:
 
-def add_client(conn, first_name, last_name, email, phones=None):
-    pass
+        # def create_db(conn):
+        #     pass
 
-def add_phone(conn, client_id, phone):
-    pass
+        # def add_client(conn, first_name, last_name, email, phones=None):
+        #     pass
 
-def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    pass
+        # def add_phone(conn, client_id, phone):
+        #     pass
 
-def delete_phone(conn, client_id, phone):
-    pass
+        # def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+        #     pass
 
-def delete_client(conn, client_id):
-    pass
+        # def delete_phone(conn, client_id, phone):
+        #     pass
 
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
-    pass
+        # def delete_client(conn, client_id):
+        #     pass
+
+        # def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+        #     pass
 
 
-with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
-    pass  # вызывайте функции здесь
+        # with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
+        #     pass  # вызывайте функции здесь
 
-conn.close()
+        # conn.close()
