9a8d0ac7fad5dbe9baa39ac468871c0a71298dbc
yi1lei6a3 <x.7machinegun@gmail.com>
2022-08-09 23:39:47 +0300

    add HomeWork 5. work postgresql with python

--- task solution/5.Работа с PostgreSQL из Python/5.Работа с PostgreSQL из Python.py
+++ task solution/5.Работа с PostgreSQL из Python/5.Работа с PostgreSQL из Python.py
@@ -1,53 +1,64 @@
 import psycopg2
 
 
+# удалить таблицы
+# def delete_tables(conn):
+
+#     cur.execute('''
+#     drop table phones;
+#     drop table users;
+#     ''')
+
 #1 создать таблицу
-def create_db(conn):
-    
+def create_tables(conn):
+
     cur.execute("""
     create table if not exists users(
     id serial primary key,
-    first_name varchar(60),
-    last_name varchar(60),
-    email varchar(60) unique);
-        
+    name varchar(60),
+    surname varchar(60),
+    email varchar(80) unique);
+    
     create table if not exists phones(
     id serial primary key,
-    users_id int references users(id on delete cascade,
+    users_id int references users(id) on delete cascade,
     phone varchar(12));
     """)
     conn.commit()
 
 #2 добавить нового клиента
-def add_client(conn, first_name, last_name, email, phones=None):
+def add_user(conn,name,surname,email,phone=None):
+
     cur.execute("""
-    insert into users(first_name,last_name,email)
+    insert into users(name,surname,email)
     values(%s,%s,%s) returning id;
-    """,(first_name,last_name,email))
+    """,(name,surname,email))
     users_id = cur.fetchone()
     print(f'{users_id=}')
     cur.execute("""
-    insert into phones(users_id,phones)
+    insert into phones(users_id,phone)
     values(%s,%s);
-    """, (users_id,phones))
+    """, (users_id,phone))
     conn.commit()
 
 #3 добавить телефон
-def add_phone(conn, first_name,last_name,email,phones):
+def add_phone(conn, name,surname,email,phone):
+
     cur.execute("""
     select id from users
-    where (first_name,last_name,email)=(%s,%s,%s);
-    """, (first_name, last_name, email))
+    where (name,surname,email)=(%s,%s,%s);
+    """, (name, surname, email))
     users_id = cur.fetchone()
     print(f'{users_id=}')
     cur.execute("""
-    insert into phone(users_id,phones)
+    insert into phones(users_id,phone)
     values(%s,%s);
-    """, (users_id, phones))
+    """, (users_id, phone))
     conn.commit()
 
 #4 изменить данные о клиенте
-def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+def update_info(conn, id, name,surname,email,phone):
+
     cur.execute("""
         update users 
         set 
@@ -55,22 +66,22 @@ def change_client(conn, client_id, first_name=None, last_name=None, email=None, 
         surname = %s,
         email = %s
         where id = %s;
-        """, (first_name, last_name, email,client_id))
+        """, (name, surname, email,id))
 
     cur.execute("""
         update phones
         set phone = %s
         where users_id = %s;
-        """, (phones,client_id))
+        """, (phone,id))
     conn.commit()
 
 #5 удалить телефон
-def delete_phone(conn, id, phones):
+def delete_phone(conn, id, phone):
 
     cur.execute("""
         delete from phones
         where users_id = %s and phone = %s
-        """, (id, phones))
+        """, (id, phone))
     conn.commit()
 
 #6 удалить клиента
@@ -83,28 +94,46 @@ def delete_user(conn, id):
     conn.commit()
 
 #7 найти клиента
-def find_user(conn, first_name=None, last_name=None, email=None, phones=None):
+def find_user(conn, name=None, surname=None, email=None, phone=None):
 
-    if phones is None:
-        
+    if phone is None:
         cur.execute("""
-            select first_name, last_name, email, phones from users
+            select name, surname, email, phone from users
             join phones on users.id = phones.users_id
-            where first_name = %s or last_name = %s or email = %s;
-            """, (first_name, last_name, email))
+            where name = %s or surname = %s or email = %s;
+            """, (name, surname, email))
         info = cur.fetchall()
         print(f'{info=}')
 
     else:
-        print('Find user with phone!')
         cur.execute("""
         select users_id from phones
-        where phone = %s;""", (phones,))
+        where phone = %s;""", (phone,))
         users_id = cur.fetchone()
 
         cur.execute("""
-                    select first_name, last_name, email, phone from users
+                    select name, surname, email, phone from users
                     join phones on users.id = phones.users_id
                     where users.id = %s;""", (users_id,))
         info = cur.fetchall()
         print(f'{info=}')
+
+
+
+
+
+with psycopg2.connect(database='netology_db',
+                        user='postgres',
+                        password='6857+Asd',
+                        host='localhost',
+                        port='5432') as conn:
+    with conn.cursor() as cur:
+        # delete_tables(conn)
+        create_tables(conn)
+        add_user(conn, 'Roman', 'Abasov', 'Abasov@gmail.com')
+        add_user(conn,'Anton','Bubnov','buben@gmail.com','+79219999999')
+        add_phone(conn,'Anton','Bubnov','buben@gmail.com','+71111111111')
+        add_user(conn,'Alina','Malina','malina@gmail.com','+78888888888')
+        add_user(conn, 'Andrey', 'Baranov', 'Baranov@gmail.com', '+71212121212')
+        update_info(conn, 1, 'Greg','Kozzlov','Kozzlov@gmail.com','22222222')
+        find_user(conn, name='Anton',phone = '+71111111111')
